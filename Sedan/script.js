74dcff0783abe3c0ac772db043b8acd41a29965c
GURUSUJAN REDDY <94339643+GURUSUJAN@users.noreply.github.com>
2023-06-02 02:03:08 +0530

    Add files via upload

---
+++ Sedan/script.js
@@ -0,0 +1,40 @@
+function myfun_tigor()
+{
+    window.open("https://cars.tatamotors.com/cars/tigor");
+}
+function myfun_verna()
+{
+    window.open("https://www.hyundai.com/in/en/find-a-car/verna/highlights.html");
+}
+function myfun_dzire()
+{
+    window.open("https://www.marutisuzuki.com/dzire");
+}
+function myfun_city()
+{
+    window.open("https://www.hondacarindia.com/honda-city-5th-generation");
+}
+function myfun_aura()
+{
+    window.open("https://www.hyundai.com/in/en/find-a-car/aura/highlights.html");
+}
+function myfun_amaze()
+{
+    window.open("https://www.hondacarindia.com/honda-amaze");
+}
+function myfun_virtus()
+{
+    window.open("https://www.volkswagen.co.in/en/models/new-virtus.html");
+}
+function myfun_skoda()
+{
+    window.open("https://mahavirskoda.com/slavia/");
+}
+function myfun_toyota()
+{
+    window.open("https://www.toyotabharat.com/showroom/camry/");
+}
+function myfun_ciaz()
+{
+    window.open("https://www.nexaexperience.com/ciaz");
+}
